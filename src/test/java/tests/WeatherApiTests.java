4ec9e50ad94c9efc11bc191f43ffee3ef58c8a46
Raaga Velthapu <vraagakrishna@gmail.com>
2025-11-01 20:09:11 +0200

    Removing test code

--- src/test/java/tests/WeatherApiTests.java
+++ src/test/java/tests/WeatherApiTests.java
@@ -1,24 +1,13 @@
 package tests;
 
-import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
-import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
-
-import static requestbuilder.WeatherApiRequestBuilder.testRequest;
 
 @Test
 @Feature("Open WeatherMap")
 @Story("Weather Station")
 public class WeatherApiTests {
 
-    @Description("As a automation tester, I want to test my framework")
-    public void test() {
-        testRequest()
-                .then()
-                .log().all()
-                .assertThat().statusCode(HttpStatus.SC_OK);
-    }
 
 }
