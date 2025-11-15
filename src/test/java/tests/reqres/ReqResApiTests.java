6761aa48ba470a0a1c7b4dbf0d3c4856e3e185f8
Raaga Velthapu <vraagakrishna@gmail.com>
2025-11-15 10:16:35 +0200

    Resert RestAssured before each test

--- src/test/java/tests/reqres/ReqResApiTests.java
+++ src/test/java/tests/reqres/ReqResApiTests.java
@@ -1,6 +1,8 @@
 package tests.reqres;
 
 import io.qameta.allure.Epic;
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeMethod;
 import utils.RandomNumberGenerator;
 import utils.ReqResTestData;
 
@@ -10,5 +12,10 @@ public class ReqResApiTests {
     ReqResTestData data = new ReqResTestData();
 
     RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+    @BeforeMethod
+    public void resetRestAssured() {
+        RestAssured.reset();
+    }
 
 }
